caadeecc12246b1142322ab358cc8b7e0ce6f213
ilyautkin <ilyautkin@mail.ru>
2014-02-17 13:21:26 +0300

    Create remove.class.php

---
+++ core/components/modextra/processors/mgr/item/remove.class.php
@@ -0,0 +1,9 @@
+<?php
+/* Чтобы не копировать то, что мы уже писали, просто подгрузим наш процессор */
+require MODX_CORE_PATH . 'components/exam/processors/sheet/remove.class.php';
+
+class modExtraItemRemoveProcessor extends ExamSheetRemoveProcessor {
+
+}
+
+return 'modExtraItemRemoveProcessor';
